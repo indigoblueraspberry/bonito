762b251bdf31b281fd75a464b19a7ff22ef83b2c
Kishwar <kishwar.shafin@gmail.com>
2019-12-18 22:11:20 -0800

    debug messages removed :fire:

--- bonito/basecaller_distributed.py
+++ bonito/basecaller_distributed.py
@@ -135,18 +135,11 @@ def chunks(file_names, chunk_length):
 def basecall(args, input_files, device_id):
     sys.stderr.write(TextColor.GREEN + "INFO: LOADING MODEL ON DEVICE: " + str(device_id) + "\n" + TextColor.END)
     model, stride, alphabet = load_model(args.model, args.config, args.gpu_mode)
-    print("GOING TO SET DEVICE")
     torch.cuda.set_device(device_id)
-    print("DEVICE SET")
     model.to(device_id)
-    print("MODEL MOVED TO CUDA")
     model.eval()
-    print("MODEL EVAL SET")
     model = DDP(model, device_ids=[device_id])
-    print("MODEL MOVED TO CUDA")
     sys.stderr.write(TextColor.GREEN + "INFO: LOADED MODEL ON DEVICE: " + str(device_id) + "\n" + TextColor.END)
-    print(model)
-    exit(1)
 
     output_directory = handle_output_directory(os.path.abspath(args.output_directory))
     fasta_file = open(output_directory + args.file_prefix + "_" + str(device_id) + ".fasta", 'w')
